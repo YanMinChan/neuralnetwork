3daf5ff802055a3391df2fc42ae93916c1cd2e82
YanMinChan <yc89@hw.ac.uk>
2025-02-25 13:41:47 +0000

    Added second hidden layer to handle non-linear case

--- toy_nn.py
+++ toy_nn.py
@@ -12,23 +12,30 @@ X = np.array([[0, 0, 1],
               [0, 1, 1],
               [1, 0, 1],
               [1, 1, 1]])
-y = np.array([[0, 0, 1, 1]]).T
+y = np.array([[0, 1, 1, 0]]).T
 
 # The NN
 def nn(input, output, activation, seed=1):
     # Forward propagation
     np.random.seed(seed)
-    w = 2*np.random.rand(3,1) - 1
-    for iter in range(10000):
+    # Assign random weight with mean 0
+    w1 = 2*np.random.rand(3,4) - 1
+    w2 = 2*np.random.rand(4,1) - 1
+    for iter in range(60000):
         l0 = input
-        l1 = activation(np.dot(input, w))
+        l1 = activation(np.dot(l0, w1))
+        l2 = activation(np.dot(l1, w2))
 
-        l1_error = output - l1
 
+        l2_error = output - l2
+        l2_delta = l2_error * activation(l2, True)
+
+        l1_error = l2_delta.dot(w2.T)
         l1_delta = l1_error * activation(l1, True)
 
-        w += np.dot(l0.T, l1_delta)
+        w1 += np.dot(l0.T, l1_delta)
+        w2 += np.dot(l1.T, l2_delta)
 
-    return l1
+    return l2
 
 print(nn(X, y, sig))
